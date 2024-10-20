ee2a945d0d72cd361c90a026b73dcfda62c1116b
alex <lescot.alexg@gmail.com>
2024-10-20 14:10:53 +0200

    navbar responsive

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function toggleSideBar() {
+    var sidebar = document.getElementById("sidebar");
+    if (sidebar.style.display === "block") {
+        sidebar.style.display = "none";
+    } else {
+        sidebar.style.display = "block";
+    }
+}
