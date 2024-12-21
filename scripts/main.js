c2f29195a33f8cebc728bd6d84a4ce3a01f0b956
Mazen-Messai <68239693+Mazen-Messai@users.noreply.github.com>
2024-12-21 21:52:39 +0100

    Supression des fichiers inutiles

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-function toggleSideBar() {
-    var sidebar = document.getElementById("sidebar");
-    if (sidebar.style.display === "block") {
-        sidebar.style.display = "none";
-    } else {
-        sidebar.style.display = "block";
-    }
-}
